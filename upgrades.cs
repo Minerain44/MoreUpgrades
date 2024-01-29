c0b971d5febc132ddf4002a5a5fd1588d9185148
Minerain44 <minerain44@gmail.com>
2024-01-29 12:15:42 +0100

    Improved Code format

--- upgrades.cs
+++ upgrades.cs
@@ -13,98 +13,98 @@ using static TerminalApi.TerminalApi;
 
 namespace MoreUpgrades
 {
-  class UpgradeManger : MonoBehaviour
-  {
-    PlayerControllerB player;
-    bool playerWasInsideFactory = false;
+    class UpgradeManger : MonoBehaviour
+    {
+        PlayerControllerB player;
+        bool playerWasInsideFactory = false;
 
-    static Postman Postman = new Postman();
-    static BiggerPockets BiggerPockets = new BiggerPockets();
-    public List<Upgrade> upgrades = new List<Upgrade>() // All currently existing upgrades
+        static Postman Postman = new Postman();
+        static BiggerPockets BiggerPockets = new BiggerPockets();
+        public List<Upgrade> upgrades = new List<Upgrade>() // All currently existing upgrades
         {
             Postman,
             BiggerPockets
         };
 
-    private void Start()
-    {
-      if (player == null)
-        player = GameNetworkManager.Instance.localPlayerController;
+        private void Start()
+        {
+            if (player == null)
+                player = GameNetworkManager.Instance.localPlayerController;
 
-      foreach (Upgrade upgrade in upgrades)
-      {
-        upgrade.Setup();
-      }
+            foreach (Upgrade upgrade in upgrades)
+            {
+                upgrade.Setup();
+            }
+        }
+
+        private void Update()
+        {
+            if (player == null)
+                player = GameNetworkManager.Instance.localPlayerController;
+
+            if (player.isInsideFactory != playerWasInsideFactory) //For Postman Upgrade
+            {
+                playerWasInsideFactory = player.isInsideFactory;
+                Postman.UpdateSpeed();
+                Postman.UpdateWeight();
+            }
+        }
     }
 
-    private void Update()
+    abstract class Upgrade
     {
-      if (player == null)
-        player = GameNetworkManager.Instance.localPlayerController;
+        int price;
+        string name;
+        string description;
+        int upgradelevel;
+        int upgradelevelCap;
 
-      if (player.isInsideFactory != playerWasInsideFactory) //For Postman Upgrade
-      {
-        playerWasInsideFactory = player.isInsideFactory;
-        Postman.UpdateSpeed();
-        Postman.UpdateWeight();
-      }
-    }
-  }
+        public int Price { get { return price; } set { price = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public string Description { get { return description; } set { description = value; } }
+        public int Upgradelevel { get { return upgradelevel; } set { upgradelevel = value; } }
+        public int UpgradelevelCap { get { return upgradelevelCap; } set { upgradelevelCap = value; } }
 
-  abstract class Upgrade
-  {
-    int price;
-    string name;
-    string description;
-    int upgradelevel;
-    int upgradelevelCap;
-
-    public int Price { get { return price; } set { price = value; } }
-    public string Name { get { return name; } set { name = value; } }
-    public string Description { get { return description; } set { description = value; } }
-    public int Upgradelevel { get { return upgradelevel; } set { upgradelevel = value; } }
-    public int UpgradelevelCap { get { return upgradelevelCap; } set { upgradelevelCap = value; } }
-
-    abstract public void Setup();
-    abstract public void LevelUp();
-  }
-
-  class Postman : Upgrade
-  {
-    bool speedUpgradeApplyed = false;
-    bool strenghUpgradeApplyed = false;
-    PlayerControllerB player;
-    float speedOffset = 0;
-    float weightOffset = 0;
-    public Postman()
-    {
-      Price = 500;
-      Name = "Postman";
-      Description = "Lets you walk faster while on the surface of the moon";
-      UpgradelevelCap = 5;
+        abstract public void Setup();
+        abstract public void LevelUp();
     }
 
-    public override void Setup()
+    class Postman : Upgrade
     {
-      throw new NotImplementedException();
-    }
+        bool speedUpgradeApplyed = false;
+        bool strenghUpgradeApplyed = false;
+        PlayerControllerB player;
+        float speedOffset = 0;
+        float weightOffset = 0;
+        public Postman()
+        {
+            Price = 500;
+            Name = "Postman";
+            Description = "Lets you walk faster while on the surface of the moon";
+            UpgradelevelCap = 5;
+        }
 
-    public void UpdateSpeed()
-    {
-      if (player.isInsideFactory && speedUpgradeApplyed)
-      {
-        player.movementSpeed -= speedOffset;
-        speedUpgradeApplyed = false;
-        Debug.Log("MoreUpgrades: Removed Speed upgrade");
-      }
-      if (!player.isInsideFactory && !speedUpgradeApplyed)
-      {
-        player.movementSpeed += speedOffset;
-        speedUpgradeApplyed = true;
-        Debug.Log("MoreUpgrades: Applyed speed upgrade");
-      }
-      Debug.Log($"MoreUpgrades: New playerspeed: {player.movementSpeed}");
-    }
+        public override void Setup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSpeed()
+        {
+            if (player.isInsideFactory && speedUpgradeApplyed)
+            {
+                player.movementSpeed -= speedOffset;
+                speedUpgradeApplyed = false;
+                Debug.Log("MoreUpgrades: Removed Speed upgrade");
+            }
+            if (!player.isInsideFactory && !speedUpgradeApplyed)
+            {
+                player.movementSpeed += speedOffset;
+                speedUpgradeApplyed = true;
+                Debug.Log("MoreUpgrades: Applyed speed upgrade");
+            }
+            Debug.Log($"MoreUpgrades: New playerspeed: {player.movementSpeed}");
+        }
 
         public void UpdateWeight()
         {
@@ -123,43 +123,43 @@ namespace MoreUpgrades
             Debug.Log($"MoreUpgrades: New Weight: {player.carryWeight}");
         }
 
-    public override void LevelUp()
-    {
-      if (player == null)
-        player = GameNetworkManager.Instance.localPlayerController;
+        public override void LevelUp()
+        {
+            if (player == null)
+                player = GameNetworkManager.Instance.localPlayerController;
 
-      speedOffset += Upgradelevel * 0.5f;
-      weightOffset += (10 - Upgradelevel) / 10;
+            speedOffset += Upgradelevel * 0.5f;
+            weightOffset += (10 - Upgradelevel) / 10;
 
-      Upgradelevel++;
-      Price += (int)MathF.Round(Price * .15f);
-      Price -= Price % 5;
-      speedUpgradeApplyed = false;
-      UpdateSpeed();
-      UpdateWeight();
-      Debug.Log($"MoreUpgrades: Leveling up Postman to level {Upgradelevel}");
-    }
-  }
-
-  class BiggerPockets : Upgrade
-  {
-    public BiggerPockets()
-    {
-      Price = 750;
-      Name = "Bigger Pockets";
-      Description = "Gives you one extra inventory slot per level";
-      UpgradelevelCap = 3;
-    }
-    public override void Setup()
-    {
-      throw new NotImplementedException();
+            Upgradelevel++;
+            Price += (int)MathF.Round(Price * .15f);
+            Price -= Price % 5;
+            speedUpgradeApplyed = false;
+            UpdateSpeed();
+            UpdateWeight();
+            Debug.Log($"MoreUpgrades: Leveling up Postman to level {Upgradelevel}");
+        }
     }
 
-    public override void LevelUp()
+    class BiggerPockets : Upgrade
     {
-      Upgradelevel++;
-      Price += (int)MathF.Round(Price * .25f);
-      Price -= Price % 5;
+        public BiggerPockets()
+        {
+            Price = 750;
+            Name = "Bigger Pockets";
+            Description = "Gives you one extra inventory slot per level";
+            UpgradelevelCap = 3;
+        }
+        public override void Setup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LevelUp()
+        {
+            Upgradelevel++;
+            Price += (int)MathF.Round(Price * .25f);
+            Price -= Price % 5;
+        }
     }
-  }
 }
