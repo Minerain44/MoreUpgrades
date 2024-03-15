3f8ad4539b310e6dad529e268baf724fd2c184db
Luca <1058086@bkrnet.de>
2024-03-15 10:14:06 +0100

    Added EnergyDrink class;

--- Items.cs
+++ Items.cs
@@ -16,5 +16,15 @@ namespace MoreUpgrades
             public string Name { get { return name; } set { name = value; } }
             public string Description { get { return description; } set { description = value; } }
         }
+
+        class EnergyDrink : Item
+        {
+            public EnergyDrink()
+            {
+                Price = 250;
+                Name = "Energy Drink";
+                Description = "A drink that increases your speed and unlimits your stamia for a short time.";
+            }
+        }
     }
 }
