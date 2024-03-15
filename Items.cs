ff94587c3de393f93947b3d5eaf3e4aad9c96960
Luca <1058086@bkrnet.de>
2024-03-15 10:11:55 +0100

    Added Base variables to Items.cs;

--- Items.cs
+++ Items.cs
@@ -6,5 +6,15 @@ namespace MoreUpgrades
 {
     internal class Items
     {
+        abstract class Item
+        {
+            int price;
+            string name;
+            string description;
+
+            public int Price { get { return price; } set { price = value; } }
+            public string Name { get { return name; } set { name = value; } }
+            public string Description { get { return description; } set { description = value; } }
+        }
     }
 }
