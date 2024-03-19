ab81d88d858a3be75559d69a990dc188c8f94230
Luca <1058086@bkrnet.de>
2024-03-19 09:22:17 +0100

    Attempted speed change

---
+++ EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.AI;
+
+namespace MoreUpgrades
+{
+    [HarmonyPatch(typeof(EnemyAI))]
+    class EnemyAIPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void UpdatePatch(EnemyAI _instance)
+        {
+            NavMeshAgent agent = _instance.GetComponent<NavMeshAgent>();
+            agent.speed = 50f;
+        }
+
+
+    }
+}
